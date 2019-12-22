c0183c56d9719f1339e1c8ec771402d6741818ee
Crispyss <vladby86@gmail.com>
2019-12-22 20:33:45 +0300

    project about christmas firs

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var obj = {
+    'one' : 1,
+    'two' : 2,
+    'three' : 3,
+    'four' : 4
+};
+
+console.log(obj[1]);
+
+// alert(`Hello world!`);
+
+
+let incr = 10,
+    decr = 10;
+
+incr;
+decr;
+
+console.log(incr);
+console.log(decr);
